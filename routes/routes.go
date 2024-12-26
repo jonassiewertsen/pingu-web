58edfeafd228ebc700024de370b52fb0649c35da
Jonas Siewertsen <jonas@pingu-solutions.com>
2024-12-26 23:05:27 +0100

    Add a little documentation for routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,20 @@ import (
 )
 
 func Init(app *application.App) {
+	/**
+	 * Add Middleware
+	 *
+	 * Check out Fiber examples:
+	 * @see https://docs.gofiber.io/next/middleware/csrf
+	 */
+	// app.Fiber.Use(csrf.New())
+
+	/**
+	 * Initialize routes
+	 *
+	 * To keep the code clean, we will initialize the routes in separate files.
+	 * This is only a suggestion, feel free doing it your way if your way of course.
+	 */
 	initWebRoutes(app)
 	// initApiRoutes(app)
 }
