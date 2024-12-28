9890d238b07aa6f41a1eaf1f69ff42202b211ce6
Jonas Siewertsen <jonas@pingu-solutions.com>
2024-12-28 20:48:48 +0100

    Template errors will be handled and will show a 500 error
    Closes #13

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"pingu-web/application"
 )
 
@@ -11,5 +12,21 @@ type Controller struct {
 func NewController(app *application.App) *Controller {
 	return &Controller{
 		App: app,
+	}
+}
+
+/**
+ * Used to render a view. On top of simply rendering the view, we also:
+ * - Pass the AppIsDev config to the view
+ */
+func (c *Controller) render(ctx *fiber.Ctx, template string, layout string, data fiber.Map) {
+	data["ViteDev"] = c.App.GetConfig().AppIsDev
+
+	err := ctx.Render(template, data, layout)
+
+	// In case an error occurred, we will log it and show a 500 error page
+	if err != nil {
+		// TODO: Log error
+		_ = ctx.Render("errors/500", fiber.Map{})
 	}
 }
--- controller/home_controller.go
+++ controller/home_controller.go
@@ -3,9 +3,10 @@ package controller
 import "github.com/gofiber/fiber/v2"
 
 func (c *Controller) Home(ctx *fiber.Ctx) error {
-	return ctx.Render("index", fiber.Map{
+	c.render(ctx, "index2", "layouts/main", fiber.Map{
 		"title":      "Hello, World!",
 		"stuff_list": []string{"a", "b", "c"},
-		"ViteDev":    c.App.GetConfig().AppIsDev,
-	}, "layouts/main")
+	})
+
+	return nil
 }
