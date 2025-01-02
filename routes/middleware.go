a9862a5acba1e07dc4e3ba7fcef0469915ffd013
Jonas Siewertsen <jonas@pingu-solutions.com>
2025-01-02 21:50:54 +0100

    Add csrf protection

--- routes/middleware.go
+++ routes/middleware.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
 	"pingu-web/application"
 	"pingu-web/routes/middleware"
@@ -13,10 +14,14 @@ import (
  * @see https://docs.gofiber.io/next/middleware/csrf
  */
 func initMiddleware(app *application.App) {
+	// Recovering from panics
 	app.Fiber.Use(middleware.RecoverMiddleware())
-	app.Fiber.Use(encryptcookie.New(encryptcookie.Config{
-		Key: app.GetConfig().CookieSecret,
-	}))
+
+	// Secure your cookies
+	app.Fiber.Use(encryptcookie.New(encryptcookie.Config{Key: app.GetConfig().CookieSecret}))
+
+	// CSRF protection
+	app.Fiber.Use(csrf.New())
 
 	// app.Fiber.Use(func(c *fiber.Ctx) error {
 	// 	c.Set("X-Request-ID", "123")
